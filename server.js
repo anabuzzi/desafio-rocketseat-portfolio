834973e3b7c36b756a83aea1e60af5c860219bc8
anabuzzi <barcelosbuzzi@gmail.com>
2020-07-29 16:44:23 -0300

    #0101 classes css dinamicas e noCache Nunjucks

--- server.js
+++ server.js
@@ -18,7 +18,22 @@ nunjucks.configure('views', {
 // até aqui o navegador procuaria o get, como o caminho não foi especificado
 // lá no navegador aparece "cannot GET"
 server.get("/", function(req, res) {
-    return res.render("about")
+    
+    const about = {
+        avatar_url: "https://i.ibb.co/G5XgTS5/Rocketseat.png",
+        name: "Rocketseat",
+        role: "As melhores tecnologias em programação, direto ao ponto e do jeito certo.",
+        description: 'No meio de tanta informação e da quantidade de ferramentas que surgem todos os dias, você precisa de alguém que te leve na direção certa. <a href="https://www.rocketseat.com.br"  target="blank">Rocketseat</a>',
+        links: [
+            {name: "Github", url: "https://github.com/Rocketseat"},
+            {name: "Linkedin", url: "https://www.linkedin.com/school/rocketseat/" },
+            {name: "Twitter", url: "https://twitter.com/rocketseat"},
+
+
+        ]
+    }
+
+    return res.render("about", { about })
 
 })
 
